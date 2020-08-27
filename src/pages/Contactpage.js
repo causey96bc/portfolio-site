7d4b20ca4195e28a31c463293183d70fdd2becbe
causey904.bc <your_email@real_email.edu>
2020-08-26 21:43:29 -0400

    deploy production

--- src/pages/Contactpage.js
+++ src/pages/Contactpage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
+// import Button from 'react-bootstrap/Button';
 
 import Hero from '../components/Hero';
 import Content from '../components/Content';
